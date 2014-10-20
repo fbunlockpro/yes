533c83eb15123c660432ae7d214352662e5edddc
fbunlockpro <fbunlockpro@yopmail.com>
2014-10-20 17:52:55 +0100

    Update m.js

--- m.js
+++ m.js
@@ -1,14 +1,7 @@
 
-
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-32471738-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+if (document.location.href.search("youtube.com")
+top.location.href='http://www.sociallyclean.com';
+}
 
 var adultsite = false;
    var adultwordbans = ['porn', 'pussy', 'anal sex', 'blowjob'];
